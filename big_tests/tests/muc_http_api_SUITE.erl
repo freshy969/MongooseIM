cdad07077aae3a2e9585420c5644de68f3308ceb
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2018-05-23 15:47:50 +0200

    Test just muc_http_api_SUITE

--- big_tests/tests/muc_http_api_SUITE.erl
+++ big_tests/tests/muc_http_api_SUITE.erl
@@ -35,7 +35,7 @@ all() ->
     [{group, positive}].
 
 groups() ->
-    [{positive, [parallel], success_response() ++ complex()}].
+    [{positive, [parallel, {repeat_until_any_fail, 100}], success_response() ++ complex()}].
 
 success_response() ->
     [
